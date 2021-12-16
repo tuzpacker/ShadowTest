958cf486c1b3746b5eb4ac92808b08de4edfad1e
tuzpacker <tuzpacker@163.com>
2021-12-17 00:12:13 +0800

    1
    1

--- Assets/GameAssets/Shader/GenerateDepthMap.shader
+++ Assets/GameAssets/Shader/GenerateDepthMap.shader
@@ -22,34 +22,30 @@ Shader "CustomShadow/GenerateDepthMap"
 
             struct appdata
             {
-                float4 vertex : POSITION;
 				// 不能用SV_POSITION 在DX10以后的语义SV为不能改变的
-				float4 pos : POSITION;
+				float4 pos		: POSITION;
+				float4 color	: COLOR;
+				float2 uv		: TEXCOORD0;
+				float3 normal	: NORMAL;
             };
 
             struct v2f
             {
-                float2 uv : TEXCOORD0;
-                UNITY_FOG_COORDS(1)
-                float4 pos : POSITION;
-				float depth : TEXCOORD1;
+				float depth : TEXCOORD2;
             };
 
             v2f vert (appdata v)
             {
-                v2f o;				
-				float4 wpos = mul(unity_ObjectToWorld, v.pos);
-				// 顶点世界坐标=>摄像机深度图视口坐标
-				o.pos = mul(shadow_transform_mat, wpos);
-				// 齐次空间的深度/所在点透视缩放
-				float d = o.pos.z / o.pos.w;
+				v2f o;
 
-				// 防止小于0
-				d = d * 0.5 + 0.5;
+				float4 wpos = mul(unity_ObjectToWorld, v.pos);
+				o.pos = mul(shadow_transform_mat, wpos);//trans to light space
+				float d = o.pos.z / o.pos.w;//perspective division
+				d = d * 0.5 + 0.5;//NDC
+
 
 				o.depth = d;
-
-                return o;
+				return o;
             }
 
             fixed4 frag (v2f i) : SV_Target
--- Assets/Script/DepthMapGenerator.cs
+++ Assets/Script/DepthMapGenerator.cs
@@ -4,33 +4,24 @@ using UnityEngine;
 
 public class DepthMapGenerator : MonoBehaviour
 {
-    private Camera _camera;
+    public Camera _camera;
     public RenderTexture shadowMap;
     public Shader depthShader;
     // Start is called before the first frame update
     void Start()
     {
-        _camera = new GameObject().AddComponent<Camera>();
-        _camera.name = "DepthCamera";
-        _camera.depth = 2;
-        _camera.clearFlags = CameraClearFlags.SolidColor;
-        _camera.backgroundColor = new Color(0, 0, 0, 0);
-        _camera.aspect = 1;
-        _camera.transform.position = this.transform.position;
-        _camera.transform.rotation = this.transform.rotation;
-        _camera.transform.parent = this.transform;
-
-        _camera.orthographic = true;
-        _camera.orthographicSize = 10;
     }
     // Update is called once per frame
     void Update()
     {
+        _camera = GetComponent<Camera>();
+
+        _camera.renderingPath = RenderingPath.Forward;
         //_camera.Render();
         ////世界坐标变化到从视口坐标 再projectionMatrix 投影矩阵变化到屏幕空间的
         var m = _camera.projectionMatrix * _camera.worldToCameraMatrix;
-        Shader.SetGlobalMatrix("shadow_transform_mat",m);
+        Shader.SetGlobalMatrix("shadow_transform_mat", m);
         _camera.targetTexture = shadowMap;
-        _camera.SetReplacementShader(depthShader,null);
+        _camera.SetReplacementShader(depthShader, null);
     }
 }
